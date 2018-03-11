5964b7bbad9c7b816d6e04868be0f47f77c2bd2f
Alex Winstanley <arwds2@gmail.com>
2018-03-11 17:44:01 -0400

    Update RaceDoor.java

--- Eminent-Domain/src/RaceDoor.java
+++ Eminent-Domain/src/RaceDoor.java
@@ -1,14 +1,45 @@
-
+	/**
+	 * @author arwinstanley
+	 * @Date 3/11/18
+	 * 
+	 * This Class is used to represent Race cards and it extends DoorCard
+	 * 
+	 */
 public class RaceDoor extends DoorCard {
-private String passive;
-public RaceDoor(String id, String desc, String pass) {
-	super(id, desc, "race");
-	passive = pass;
-}
-public String getPassive() {
-	return passive;
-}
-public void setPassive(String passive) {
-	this.passive = passive;
-}
+	// A string desc of the passive trait
+	private String passive;
+	// sets the type to race for all of these cards
+	private static final String TYP = "race";
+	/**
+	 * @param id is the id
+	 * @param desc is the description
+	 * @param pass is the desc of the passive trait
+	 * 
+	 * fairly standard constructor, calls its super
+	 * 
+	 */
+	public RaceDoor(String id, String desc, String pass) {
+		super(id, desc, TYP);
+		passive = pass;
+	}
+	/**
+	* @param none
+	* @return passive
+	* 
+	* fairly standard getter
+	* 
+	*/
+	public String getPassive() {
+		return passive;
+	}
+	/**
+	* @param board is the game board
+	* @return none
+	* 
+	* checks the board state against the passive ability
+	* 
+	*/
+	public void checkPassive(Board board) {
+	
+	}
 }
