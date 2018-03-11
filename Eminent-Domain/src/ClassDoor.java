c73c1984e02ff4811a7e3de42eaedb7da992719e
Alex Winstanley <arwds2@gmail.com>
2018-03-11 17:46:14 -0400

    Update ClassDoor.java

--- Eminent-Domain/src/ClassDoor.java
+++ Eminent-Domain/src/ClassDoor.java
@@ -1,22 +1,22 @@
-
+	/**
+	 * @author arwinstanley
+	 * @Date 3/11/18
+	 * 
+	 * This Class is used to represent Class cards and it extends DoorCard
+	 * 
+	 */
 public class ClassDoor extends DoorCard {
-private String primary;
-private String secondary;
-public ClassDoor(String id, String desc, String prime, String seco) {
-	super(id, desc, "class");
-	primary = prime;
-	secondary = seco;
-}
-public String getPrimary() {
-	return primary;
-}
-public void setPrimary(String primary) {
-	this.primary = primary;
-}
-public String getSecondary() {
-	return secondary;
-}
-public void setSecondary(String secondary) {
-	this.secondary = secondary;
-}
+	private String primary;
+	private String secondary;
+	public ClassDoor(String id, String desc, String prime, String seco) {
+		super(id, desc, "class");
+		primary = prime;
+		secondary = seco;
+	}
+	public String getPrimary() {
+		return primary;
+	}
+	public String getSecondary() {
+		return secondary;
+	}
 }
