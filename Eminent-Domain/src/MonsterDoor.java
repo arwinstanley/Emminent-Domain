4142d4f08ed63e4f7463fb284a7e8a3cbdb07f7f
Alex Winstanley <arwds2@gmail.com>
2018-03-11 17:43:18 -0400

    Update MonsterDoor.java

--- Eminent-Domain/src/MonsterDoor.java
+++ Eminent-Domain/src/MonsterDoor.java
@@ -1,53 +1,88 @@
-
+	/**
+	 * @author arwinstanley
+	 * @Date 3/11/18
+	 * 
+	 * This Class is used to represent monster cards and it extends DoorCard
+	 * 
+	 */
 public class MonsterDoor extends DoorCard  {
-private int treasure;
-private int gainedLvls;
-private int level;
-private String badStuff;
-private boolean alive = false;
-private int debuff = 0;
-public int getDebuff() {
-	return debuff;
-}
-public void setDebuff(int debuff) {
-	this.debuff = debuff;
-}
-public MonsterDoor(String id, String desc, int tres, int lvlsUp, int lvl, String bad) {
-	super(id, desc,"monster");
-	treasure = tres;
-	gainedLvls = lvlsUp;
-	level = lvl;
-	badStuff = bad;
-}
-public Monster summon() {
-	if(alive)
-		return null;
-	alive = true;
-	return new Monster(this);
+	// amount of treasure gained
+	private int treasure;
+	// amaount of lvls gained
+	private int gainedLvls;
+	// creatures lvl
+	private int level;
+	// punishment for defeat
+	private String badStuff;
+	// sets type to "monster" for all monster cards
+	private static final String TYP = "monster";
 	
-}
-public int getTreasure() {
-	return treasure;
-}
-public void setTreasure(int treasure) {
-	this.treasure = treasure;
-}
-public int getGainedLvls() {
-	return gainedLvls;
-}
-public void setGainedLvls(int gainedLvls) {
-	this.gainedLvls = gainedLvls;
-}
-public int getLevel() {
-	return level;
-}
-public void setLevel(int level) {
-	this.level = level;
-}
-public String getBadStuff() {
-	return badStuff;
-}
-public void setBadStuff(String badStuff) {
-	this.badStuff = badStuff;
-}
+	/**
+	 * @param id is the id
+	 * @param desc is the description
+	 * @param tres is tresure gained
+	 * @param lvlsUp is the lvls gained
+	 * @param lvl is the monsters lvl
+	 * @param bad is the punishment 
+	 * @return none
+	 * 
+	 * fairly standard constructor, calls its super
+	 * 
+	 */
+	public MonsterDoor(String id, String desc, int tres, int lvlsUp, int lvl, String bad) {
+		super(id, desc,TYP);
+		treasure = tres;
+		gainedLvls = lvlsUp;
+		level = lvl;
+		badStuff = bad;
+	}	/**
+	* @param none
+	* @return new Monster with the source being 'this' card
+	* 
+	* Creates an Entity Monster from this Card
+	* 
+	*/
+	public Monster summon() {
+		return new Monster(this);
+	}
+	/**
+	* @param none
+	* @return treasure
+	* 
+	* fairly standard getter
+	* 
+	*/
+	public int getTreasure() {
+		return treasure;
+	}
+	/**
+	* @param none
+	* @return gainedLevels
+	* 
+	* fairly standard getter
+	* 
+	*/
+	public int getGainedLvls() {
+		return gainedLvls;
+	}
+	/**
+	* @param none
+	* @return card lvl
+	* 
+	* fairly standard getter
+	* 
+	*/
+	public int getLevel() {
+		return level;
+	}
+	/**
+	* @param none
+	* @return the bad stuff
+	* 
+	* fairly standard getter
+	* 
+	*/
+	public String getBadStuff() {
+		return badStuff;
+	}
 }
